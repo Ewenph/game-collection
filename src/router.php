b96229b7a41176d0a84f78af2810ac8a306c8529
Romain <romainthebault22100@gmail.com>
2024-12-10 14:09:25 +0100

    setup file

---
+++ src/router.php
@@ -0,0 +1,21 @@
+<?php
+function routeRequest() {
+    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+    switch ($uri) {
+        case '/':
+            require_once __DIR__ . '/controllers/HomeController.php';
+            break;
+        case '/login':
+            require_once __DIR__ . '/controllers/LoginController.php';
+            break;
+        case '/register':
+            require_once __DIR__ . '/controllers/RegisterController.php';
+            break;
+        // Add more routes as needed
+        default:
+            http_response_code(404);
+            echo "Page not found";
+            break;
+    }
+}
