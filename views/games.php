52f940ab5ccf359ff0be33b2be5a2fe77e0fbd08
hadys72 <155895104+hadys72@users.noreply.github.com>
2024-12-10 16:28:15 +0100

    ajout de la page de jeux

--- views/games.php
+++ views/games.php
@@ -4,6 +4,24 @@
     <title>Jeux</title>
 </head>
 <body>
-    <h1>Jeux</h1>
+<h1>Classement des temps passés</h1>
+<table>
+        <thead>
+            <tr>
+                <th>Joueur</th>
+                <th>Temps_passés</th>
+                <th>Jeu favori</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($jeux as $jeu): ?>
+                <tr>
+                    <td><?= htmlspecialchars($jeu['Id_uti']) ?></td>
+                    <td><?= htmlspecialchars($jeu['Temps_jeu']) ?></td>
+                    <td><?= htmlspecialchars($jeu['Editeur_jeu']) ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 </body>
 </html>
