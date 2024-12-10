3b02269d0a44dd0d5348a4d028be205f5f8392e6
Romain <romainthebault22100@gmail.com>
2024-12-10 15:17:21 +0100

    Update router.php

--- src/router.php
+++ src/router.php
@@ -5,12 +5,58 @@ function routeRequest() {
     switch ($uri) {
         case '/':
             require_once __DIR__ . '/controllers/HomeController.php';
+            $controller = new HomeController();
+            $controller->index();
             break;
         case '/login':
             require_once __DIR__ . '/controllers/LoginController.php';
+            $controller = new LoginController();
+            $controller->showLoginForm();
+            break;
+        case '/login/submit':
+            require_once __DIR__ . '/controllers/LoginController.php';
+            $controller = new LoginController();
+            $controller->login();
             break;
         case '/register':
             require_once __DIR__ . '/controllers/RegisterController.php';
+            $controller = new RegisterController();
+            $controller->showRegisterForm();
+            break;
+        case '/register/submit':
+            require_once __DIR__ . '/controllers/RegisterController.php';
+            $controller = new RegisterController();
+            $controller->register();
+            break;
+        case '/logout':
+            require_once __DIR__ . '/controllers/LoginController.php';
+            $controller = new LoginController();
+            $controller->logout();
+            break;
+        case '/games':
+            require_once __DIR__ . '/controllers/GameController.php';
+            $controller = new GameController();
+            $controller->index();
+            break;
+        case '/games/add':
+            require_once __DIR__ . '/controllers/GameController.php';
+            $controller = new GameController();
+            $controller->showAddGameForm();
+            break;
+        case '/games/add/submit':
+            require_once __DIR__ . '/controllers/GameController.php';
+            $controller = new GameController();
+            $controller->addGame();
+            break;
+        case '/games/delete':
+            require_once __DIR__ . '/controllers/GameController.php';
+            $controller = new GameController();
+            $controller->deleteGame();
+            break;
+        case '/leaderboard':
+            require_once __DIR__ . '/controllers/LeaderboardController.php';
+            $controller = new LeaderboardController();
+            $controller->index();
             break;
         default:
             http_response_code(404);
