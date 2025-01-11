024909003d8bcb67ca5bed313de3defe8e6a4827
Romain <romainthebault22100@gmail.com>
2025-01-11 21:05:54 +0100

    page 404

---
+++ views/404.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page non trouvée</title>
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/game-collection/views/style/404.css">
+</head>
+<body>
+    <div class="container">
+        <h1>404</h1>
+        <p>Oups! La page que vous recherchez n'existe pas.</p>
+        <a href="/game-collection/views/home.php">Retour à l'accueil</a>
+    </div>
+</body>
+</html>
