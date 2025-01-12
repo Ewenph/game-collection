49c60453af34f0be3176eee959dc5001a2ff4831
Romain <romainthebault22100@gmail.com>
2025-01-12 01:29:38 +0100

    Update leaderboard.php

--- views/leaderboard.php
+++ views/leaderboard.php
@@ -12,7 +12,7 @@ require_once __DIR__ . '/header.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Classement</title>
-    <link rel="stylesheet" href="/game-collection/views/style/common.css">
+    <link rel="stylesheet" href="/views/style/common.css">
 </head>
 <body>
 <main>
